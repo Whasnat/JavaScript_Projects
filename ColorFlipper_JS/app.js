f86a683a198845b2c304043a9bad169f0d08f07d
w.hasnat94@gmail.com <git94w.hasnat>
2021-03-06 14:52:29 +0600

    simple done

--- ColorFlipper_JS/app.js
+++ ColorFlipper_JS/app.js
@@ -1,1 +1,14 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "Aqua", "AntiqueWhite", "Aquamarine", "Blue","Crimson", "DarkMagenta", "Indigo", "Maroon", "Navy"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+
+    randomNumber = Math.floor(Math.random() *colors.length) +1; 
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+
+});
+
+
