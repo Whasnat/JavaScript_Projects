b980ebda04252de085ffd6b9a266bc36d9967623
w.hasnat94@gmail.com <git94w.hasnat>
2021-03-08 00:05:46 +0600

    Count 0.1

--- ColorFlipper_JS/app.js
+++ ColorFlipper_JS/app.js
@@ -4,11 +4,9 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function(){
-
     let randomNumber = Math.floor(Math.random() *colors.length) +1; 
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
-
 });
 
 
