e058f2a0a71d5a2a7e8293edc2ba36aa999a79c8
Whasnat <w.hasnat94@gmail.com>
2021-03-15 01:26:46 +0600

    Review done

--- Review/app.js
+++ Review/app.js
@@ -7,7 +7,8 @@ const reviews = [
     img:
       "https://scontent.fcgp17-1.fna.fbcdn.net/v/t1.0-9/60674163_2227034044043889_2409629068560433152_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=174925&_nc_eui2=AeGeq15EvLpoCA5ayn_1Bf4LK-GUKEICOdkr4ZQoQgI52aimaVezWPyzGxTzGDZslarQ_iFTLL2iNhu4kNCMXv-D&_nc_ohc=uH3eLJCaJbAAX9FfuZ2&_nc_ht=scontent.fcgp17-1.fna&oh=b9479e3c4d6672290beb6a9a56870df9&oe=607216EE",  
     text:
-    "He is Ananta Jalil, also known as Jalil Kaku. He doesn't care if you are Sakib Khan or Amir Khan. He considers Tom Cruse to be his only competitor. His job is to make the impossible possible."  },
+      "He is Ananta Jalil, also known as Jalil Kaku. He doesn't care if you are Sakib Khan or Amir Khan. He considers Tom Cruse to be his only competitor. His job is to make the impossible possible.",
+  },
   {
     id: 2,
     name: "anna johnson",
@@ -43,14 +44,44 @@ const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
 
-const prevBtn = getElementById(".prev-btn");
-const nextBtn = getElementById(".next-btn");
-const randomBtn = getElementById(".random-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
 
 // Initial Item
 let currentItem = 0;
 
+// First Load window
 window.addEventListener("DOMContentLoaded", function(){
-  const item = reviews[currentItem];
+  displayReview();
+});
 
+
+// Display Individual Review
+function displayReview(){
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+
+// Button Clicks
+nextBtn.addEventListener("click", function(){
+  currentItem ++;
+  if(currentItem > reviews.length - 1) // if currentItem is at the last item
+    currentItem = 0;
+  displayReview();
+});
+prevBtn.addEventListener("click", function(){
+  currentItem --;
+  if(currentItem < 0 )    // if currentItem is at the first item
+    currentItem = reviews.length -1;  
+  displayReview();
+});
+
+randomBtn.addEventListener("click", function(){
+  currentItem = Math.floor(Math.random() * reviews.length); //generate a random within reviews.length.
+  displayReview();
 });
