297b7285666b307ee0ab05dd0d9c9f415b47ce19
Whasnat <w.hasnat94@gmail.com>
2021-03-16 22:49:46 +0600

    Toggle function JS

--- Sidebar/app.js
+++ Sidebar/app.js
@@ -3,11 +3,12 @@ const sidebar = document.querySelector(".sidebar");
 const closeBtn = document.querySelector(".close-btn");
 
 sidebarToggle.addEventListener("click", function(){
-    if (sidebar.classList.contains("show-sidebar")){
-        sidebar.classList.remove("show-sidebar");
-    }else{
-        sidebar.classList.add("show-sidebar");
-    }
+    // if (sidebar.classList.contains("show-sidebar")){
+    //     sidebar.classList.remove("show-sidebar");
+    // }else{
+    //     sidebar.classList.add("show-sidebar");
+    // }
+    sidebar.classList.toggle("show-sidebar");
 });
 
 closeBtn.addEventListener("click", function(){
